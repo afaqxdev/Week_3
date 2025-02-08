81be206a4a730be5cc568536425c14be22e26398
Afaq Zahir <116549225+MetasenseDevelopment@users.noreply.github.com>
2025-02-08 14:52:34 +0500

    add customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:week_3/listview_exapmle.dart';
+import 'package:week_3/theme_changer.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isThemeMode = true;
+
+  ThemeData lightThem = ThemeData(
+      brightness: Brightness.light,
+      appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+      textTheme: const TextTheme(
+        bodySmall: TextStyle(color: Colors.black, fontSize: 50),
+      ));
+  ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      textTheme: const TextTheme(
+        bodySmall: TextStyle(color: Colors.red),
+      ));
   @override
   Widget build(BuildContext context) {
+    print(isThemeMode);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: ListViewExample(),
-    );
+        theme: isThemeMode ? lightThem : darkTheme,
+        title: 'Flutter Demo',
+        home: ThemeChanger(
+          isTheme: isThemeMode,
+          onChanged: (value) {
+            isThemeMode = value;
+            setState(() {});
+            print(value);
+          },
+        ));
   }
 }
